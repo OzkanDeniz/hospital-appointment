b538bdb1399509893c750f8197ed6b86c301d52a
ozkan <ozkandnz09@gmail.com>
2024-11-08 00:13:27 +0300

    useState lifting up doctors component

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -3,18 +3,14 @@ import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function AddModal() {
-  const [show, setShow] = useState(false);
+function AddModal({handleClose,show}) {
+  //   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  //   const handleClose = () => setShow(false);
+  //   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Appoinment for drname</Modal.Title>
@@ -22,12 +18,12 @@ function AddModal() {
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="name">
-              <Form.Label>Patient Name</Form.Label>
+              <Form.Label>Patient Name:</Form.Label>
               <Form.Control type="text" placeholder="Enter Your Name" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="date">
-              <Form.Label>Day&Time</Form.Label>
+              <Form.Label>Day&Time:</Form.Label>
               <Form.Control type="datetime-local" placeholder="date" />
             </Form.Group>
             <Form.Group
@@ -35,10 +31,10 @@ function AddModal() {
               controlId="formBasicCheckbox"
             ></Form.Group>
             <div className="text-center ">
-              <Button variant="success" onClick={handleClose}>
+              <Button type="submit" variant="success" className="me-2">
                 Save
               </Button>
-              <Button variant="danger" onClick={handleClose}>
+              <Button type="button" variant="danger" onClick={handleClose}>
                 Close
               </Button>
             </div>
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -2,8 +2,14 @@ import Container from "react-bootstrap/Container";
 import { doctorData } from "../helper/data";
 import { Col, Row } from "react-bootstrap";
 import AddModal from "./AddModal";
+import { useState } from "react";
 
 const Doctors = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -12,7 +18,7 @@ const Doctors = () => {
       <Row>
         {doctorData.map(({ id, name, dep, img }) => (
           <Col xs={6} md={4} lg={3} key={id}>
-            <img src={img} alt={name} className="doctor-img img-thumbnail" />
+            <img src={img} alt={name} className="doctor-img img-thumbnail" onClick={handleShow} />
             <h5>{name}</h5>
             <h6>{dep}</h6>
           </Col>
@@ -26,7 +32,7 @@ const Doctors = () => {
           // );
         ))}
       </Row>
-      <AddModal/>
+      <AddModal handleClose={handleClose} show={show}/>
     </Container>
   );
 };
